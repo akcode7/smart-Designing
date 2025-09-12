4a61a52ed989ae2d95699a4d15b05de8b8f479b5
Ankit Sharma <ankitengineer@hotmail.com>
2025-09-13 01:23:05 +0530

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+    build: {
+            rollupOptions: {
+              input: {
+                main: 'index.html',
+                admin: 'Admin_dash.html',
+                judging: 'judging.html'
+              },
+            },
+          },
 });
+
