49b03982dd4f65ea360cf674ed4f8207af78eea4
Ankit Sharma <ankitengineer@hotmail.com>
2025-09-13 22:51:31 +0530

    new push now

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
             rollupOptions: {
               input: {
                 main: 'index.html',
-                admin: 'admin_dash.html',
+                admin: 'Admin_dash.html',
                 judging: 'judging.html'
               },
             },
